56da64a6233e911f539592ab71264055280d5696
CabaVS <valerii.savchenko@icloud.com>
2025-09-26 12:07:54 +0200

    Integrate OpenTelemetry Collector for tracing and metrics in Aspire project for Keycloak

--- aspire/CabaVS.Aspire/AppHost.cs
+++ aspire/CabaVS.Aspire/AppHost.cs
@@ -1,6 +1,7 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
 IResourceBuilder<ParameterResource> sqlPassword = builder.AddParameter("SqlPassword", secret: true);
+IResourceBuilder<ParameterResource> otelCollectorConfigFullPath = builder.AddParameter("OtelCollectorConfigFullPath", secret: true);
 IResourceBuilder<ParameterResource> keycloakUsername = builder.AddParameter("KeycloakUsername");
 IResourceBuilder<ParameterResource> keycloakPassword = builder.AddParameter("KeycloakPassword", secret: true);
 
@@ -10,6 +11,17 @@ IResourceBuilder<SqlServerServerResource> sql = builder
     .WithLifetime(ContainerLifetime.Persistent);
 
 IResourceBuilder<SqlServerDatabaseResource> keycloakDb = sql.AddDatabase("cvs-idx-keycloak-sqldb");
+
+IResourceBuilder<ContainerResource> otelCollector = builder
+    .AddContainer("cvs-idx-otel-collector", image: "otel/opentelemetry-collector:0.104.0")
+    .WithBindMount(
+        await otelCollectorConfigFullPath.Resource.GetValueAsync(CancellationToken.None)
+        ?? throw new InvalidOperationException("OTEL Collector Config Full Path not found."), 
+        "/etc/otelcol/config.yaml")
+    .WithEnvironment("ASPIRE_OTLP_API_KEY", builder.Configuration["AppHost:OtlpApiKey"])
+    .WithArgs("--config", "/etc/otelcol/config.yaml")
+    .WithEndpoint(name: "otlp-grpc", port: 4317, targetPort: 4317)
+    .WithHttpEndpoint(name: "otlp-http", port: 4318, targetPort: 4318);
 
 _ = builder
     .AddKeycloak("cvs-idx-keycloak-ca", port: 5010, keycloakUsername, keycloakPassword)
@@ -27,7 +39,14 @@ _ = builder
     })
     .WithEnvironment("KC_HTTP_ENABLED", "true")
     .WithEnvironment("KC_HOSTNAME_STRICT", "false")
+    .WithEnvironment("KC_TRACING_ENABLED", "true")
+    .WithEnvironment("KC_TRACING_PROTOCOL", "grpc")
+    .WithEnvironment("KC_TRACING_ENDPOINT", () => $"http://{otelCollector.Resource.Name}:4317")
+    .WithEnvironment("KC_TRACING_SERVICE_NAME", "cvs-idx-keycloak-ca")
+    .WithEnvironment("KC_TRACING_SAMPLER_RATIO", "1.0")
+    .WithEnvironment("KC_METRICS_ENABLED", "true")
     .WithArgs("start-dev --auto-build")
-    .WaitFor(keycloakDb);
+    .WaitFor(keycloakDb)
+    .WaitFor(otelCollector);
 
 await builder.Build().RunAsync();
